6e1b9f03ebcecd6baf9dd6b65c1f8d7ec20149a9
yoh2 <yoh2.sdj@gmail.com>
2022-04-24 12:30:06 +0900

    feat: make Ook! implementation to follow TokenInfo changes.

--- src/predefined/ook.rs
+++ src/predefined/ook.rs
@@ -4,7 +4,7 @@
 use crate::{
     error::ParseError,
     prelude::Parser,
-    token::{TokenInfo, TokenStream, TokenType, Tokenizer},
+    token::{Token, TokenInfo, TokenStream, TokenType, Tokenizer},
 };
 
 #[derive(Debug, Clone, Copy)]
@@ -19,6 +19,7 @@ enum OokTokenType {
 
 struct OokTokenInfo {
     token_type: Option<OokTokenType>,
+    pos: usize,
     /// The position of the token in the source.
     pos_in_chars: usize,
 }
@@ -73,6 +74,7 @@ impl<'a> OokTokenStream<'a> {
                 };
                 let info = OokTokenInfo {
                     token_type: Some(token_type),
+                    pos: self.pos + rel_pos,
                     pos_in_chars: self.pos_in_chars + rel_pos_in_chars,
                 };
                 // next position
@@ -90,29 +92,30 @@ impl<'a> OokTokenStream<'a> {
 
         OokTokenInfo {
             token_type: None,
+            pos: self.pos,
             pos_in_chars: self.pos_in_chars,
         }
     }
 }
 
-impl<'a> TokenStream for OokTokenStream<'a> {
-    fn next(&mut self) -> Result<TokenInfo, ParseError> {
-        let (first_token_type, first_token_pos_in_chars) = {
+impl<'a> TokenStream<'a> for OokTokenStream<'a> {
+    fn next(&mut self) -> Result<TokenInfo<'a>, ParseError> {
+        let (first_token_type, first_token_pos, first_token_pos_in_chars) = {
             let token = self.next_ook_token();
             if let Some(token_type) = token.token_type {
-                (token_type, token.pos_in_chars)
+                (token_type, token.pos, token.pos_in_chars)
             } else {
                 return Ok(TokenInfo {
-                    token_type: None,
+                    token: None,
                     pos_in_chars: token.pos_in_chars,
                 });
             }
         };
 
-        let second_token_type = {
+        let (second_token_type, second_token_pos) = {
             let token = self.next_ook_token();
             if let Some(token_type) = token.token_type {
-                token_type
+                (token_type, token.pos)
             } else {
                 return Err(ParseError::MiscError(
                     token.pos_in_chars,
@@ -139,7 +142,11 @@ impl<'a> TokenStream for OokTokenStream<'a> {
         };
 
         Ok(TokenInfo {
-            token_type: Some(token_type),
+            token: Some(Token {
+                token_type,
+                token_str: &self.source
+                    [first_token_pos..second_token_pos + COMMON_TOKEN_PART.len() + 1],
+            }),
             pos_in_chars: first_token_pos_in_chars,
         })
     }
@@ -149,6 +156,30 @@ impl<'a> TokenStream for OokTokenStream<'a> {
 mod test {
     use super::*;
     use crate::runtime;
+
+    #[test]
+    fn test_token_stream() {
+        // byte 10 (char 4): PInc
+        // byte 43 (char 25): EOF
+        let mut stream = OokTokenizer.token_stream("ＡＢＣ Ook. ＤＥＦ Ook? Oo..ＸＹＺ");
+        assert_eq!(
+            stream.next().unwrap(),
+            TokenInfo {
+                token: Some(Token {
+                    token_type: TokenType::PInc,
+                    token_str: "Ook. ＤＥＦ Ook?",
+                }),
+                pos_in_chars: 4,
+            },
+        );
+        assert_eq!(
+            stream.next().unwrap(),
+            TokenInfo {
+                token: None,
+                pos_in_chars: 25,
+            },
+        );
+    }
 
     #[test]
     fn test_hello_world() {
